b89f84021b08f0fb337366b5f021501ae196f796
jslhost <jean.si@hotmail.fr>
2024-02-21 01:21:04 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My test project',
+    author='Jean-Simon',
+    license='MIT',
+)
